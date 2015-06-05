4ca2f8569c6c40132ffe116db87d88510bf95569
Sara OC <saramoo@hotmail.com>
2015-06-05 13:57:58 +0100

    Initial Commit

---
+++ StanfordCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StanfordCalculator
+//
+//  Created by Sara OC on 05/06/2015.
+//  Copyright (c) 2015 Sara OC Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
