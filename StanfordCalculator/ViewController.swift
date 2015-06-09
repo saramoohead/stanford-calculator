536550d6b0ee8663d9f2d96fec1281931e60118c
Sara OC <saramoo@hotmail.com>
2015-06-09 09:03:02 +0100

    Working calculator, added decimal point, auto-layout in place

--- StanfordCalculator/ViewController.swift
+++ StanfordCalculator/ViewController.swift
@@ -10,16 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    var userHasEnteredAFloatingPoint = false
+    
+    @IBAction func appendFloatingPoint(sender: UIButton) {
+        if !userHasEnteredAFloatingPoint {
+            display.text = display.text! + sender.currentTitle!
+            userHasEnteredAFloatingPoint = true
+        } else {}
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+            case "×": performOperation { $0 * $1 }
+            case "÷": performOperation { $1 / $0 }
+            case "+": performOperation { $0 + $1 }
+            case "−": performOperation { $1 - $0 }
+            case "√": performOperation { sqrt($0) }
+            default: break
+        }
     }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if internalStackOfNumbers.count >= 2 {
+            displayValue = operation(internalStackOfNumbers.removeLast(), internalStackOfNumbers.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if internalStackOfNumbers.count >= 1 {
+            displayValue = operation(internalStackOfNumbers.removeLast())
+            enter()
+        }
+    }
+    
+    var internalStackOfNumbers = Array<Double>()
 
-
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        userHasEnteredAFloatingPoint = false
+        internalStackOfNumbers.append(displayValue)
+        println("internalStack = \(internalStackOfNumbers)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text! = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
 
